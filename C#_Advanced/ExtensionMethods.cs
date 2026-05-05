342f0da6ef11007d9abe190eedfd9ffafa8dacfb
Haidy Gabal <haidy.a.gabal2024@gmail.com>
2026-05-06 02:44:38 +0300

    add ExtensionMethods

---
+++ C#_Advanced/ExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Advanced
+{
+    public static class ExtensionMethods
+    {
+        ///1- Extension Methods
+        ///Extension methods allow you to add new methods to existing types without modifying the original type,
+        ///creating a new derived type, or recompiling.
+        ///
+        /// first you must static for class
+        /// add this keyword for first parameter
+        /// 
+
+        public static bool checkNumberBetween(this int num,int first,int last)
+        {
+            return num >= first && num <= last;
+        }
+
+    }
+}
---
+++ C#_Advanced/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace C__Advanced
+{
+    internal class Program
+    {
+   
+        static void Main(string[] args)
+        {
+            /// 1-Extension Methods
+            void ExtensionMethods()
+            {
+                int number = 14;
+
+                if (number.checkNumberBetween(10, 40))
+                    Console.WriteLine("Is Between");
+                else
+                    Console.WriteLine("Is Not");
+            }
+            ExtensionMethods();
+
+
+
+
+
+
+
+        }
+    }
+}
