0bad4d054f5f35ed47093320bd761a5c3d04c69e
Haidy Gabal <haidy.a.gabal2024@gmail.com>
2026-06-02 00:21:54 +0300

    add TBA

---
+++ C#_Advanced/Task-BasedAsynchronousPattern.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Advanced
+{
+    internal class Task_BasedAsynchronousPattern
+    {
+        Return a Task or Task<TResult>
+
+Use async / await for natural consumption
+
+Follow naming convention MethodNameAsync
+
+Report progress via IProgress<T> if needed
+
+Support cancellation via CancellationToken
+    }
+}
