d98028219c4c0b0d40f0b59e7032198198cd3fc1
Haidy Gabal <haidy.a.gabal2024@gmail.com>
2026-07-31 23:23:03 +0300

    Use async / await

--- C#_Advanced/Task-BasedAsynchronousPattern.cs
+++ C#_Advanced/Task-BasedAsynchronousPattern.cs
@@ -9,9 +9,28 @@ namespace C__Advanced
 {
     internal class Task_BasedAsynchronousPattern
     {
-       // Return a Task or Task<TResult>
+        // Return a Task or Task<TResult>
+        public async Task SaveDataAsync()
+        {
+            await Task.Delay(1000);
+        }
+
+        public async Task<string> GetUserNameAsync()
+        {
+            await Task.Delay(1000);
+            return "Haidy";
+        }
 
         //Use async / await for natural consumption
+        public async Task DownloadDataAsync()
+        {
+            Console.WriteLine("Downloading...");
+
+            // Simulate a 2-second operation
+            await Task.Delay(2000);
+
+            Console.WriteLine("Download completed.");
+        }
 
         //Follow naming convention MethodNameAsync
 
